f6dcb89c42c38bdab90f66acfef19ce68c8c682c
waldenhillegass <waldenhillegass@gmail.com>
2023-01-15 14:58:41 -0800

    yee

--- src/raspberry_pi_transmitter/radio_transmitter.py
+++ src/raspberry_pi_transmitter/radio_transmitter.py
@@ -6,7 +6,7 @@ from time import sleep
 from radio import Radio
 from Packet import Packet
 def main():
-    radio = Radio('/dev/serial0', 4, 5, 6)
+    radio = Radio('/dev/serial0', 7, 11, 12)
     while True:
         p = Packet(5000000, b'It was the best of times, it was the worst of times')
         radio.send(p.get_bytes())
