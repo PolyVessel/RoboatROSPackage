eec188da5fe7becff1619be7d6ea8f2e37b4d89a
waldenhillegass <waldenhillegass@gmail.com>
2023-02-04 12:45:22 -0800

    .

--- src/raspberry_pi_transmitter/radio_transmitter.py
+++ src/raspberry_pi_transmitter/radio_transmitter.py
@@ -6,6 +6,12 @@ from time import sleep
 from radio import Radio
 from Packet import Packet
 def main():
+    if not radio.ping_radio():
+        print("Radio self-test failed!")
+        return
+    else:
+        print("Radio self-test passed!")
+
     radio = Radio('/dev/ttyAMA0', 7, 11, 12)
     i = 1
     while True:
