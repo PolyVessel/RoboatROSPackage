99d757769029ea89e3410584a14ecef1c121ff6a
waldenhillegass <waldenhillegass@gmail.com>
2023-02-11 11:59:48 -0800

    yes

--- src/raspberry_pi_transmitter/radio_transmitter.py
+++ src/raspberry_pi_transmitter/radio_transmitter.py
@@ -7,11 +7,13 @@ from radio import Radio
 from Packet import Packet
 def main():
     radio = Radio('/dev/serial0', 7, 11, 12)
+    i = 0
     while True:
         p = Packet(i, b'It was the best of times, it was the worst of times')
         radio.transmit(p.get_bytes())
         print(i)
         sleep(1)
+        i += 1
 
 
 if __name__ == "__main__":
