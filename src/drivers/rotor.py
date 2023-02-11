bca32ad39a14bb0931a7d88d8dea0da92ec0aa3e
Dylan Starink <dylan@starink.com>
2023-02-10 19:46:53 -0800

    Move sleep to prevent PRU init warning

--- src/drivers/rotor.py
+++ src/drivers/rotor.py
@@ -24,12 +24,13 @@ class Rotor:
         rcpy.set_state(rcpy.RUNNING)
         self.esc = ESC(self.channel)
 
-        # Wait for PRU Init
-        time.sleep(1)
+        
 
         # Start the Clock for the escs
         self.esc.start(0.02)
 
+        # Wait for PRU Init
+        time.sleep(1)
 
         # Arm ESC
         self.set(-0.1)
