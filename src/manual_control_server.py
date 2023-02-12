4b47666e4bd23a6bf5fdc2abe4728c4919496340
Dylan Starink <dylan@starink.com>
2023-02-11 20:42:32 -0800

    Fix Naming for ManualControl

--- src/manual_control_server.py
+++ src/manual_control_server.py
@@ -2,14 +2,14 @@
 
 import rospy
 import actionlib
-from roboat_pkg.msg import GPSInfo
+from roboat_pkg.msg import ManualControlAction
 from std_msgs.msg import Float32, Bool
 
 class ManualControlAction:
 
     def __init__(self, name):
         self._action_name = name
-        self._as = actionlib.SimpleActionServer(self._action_name, roboat_pkg.msg.ManualControlAction, execute_cb=self.execute_cb, auto_start = False)
+        self._as = actionlib.SimpleActionServer(self._action_name, ManualControlAction, execute_cb=self.execute_cb, auto_start = False)
 
         rospy.Subscriber('/user/throttle', Float32, self.user_throttle_cb)
         self._throttle = 0.0 # Default Value for Throttle
@@ -22,18 +22,28 @@ class ManualControlAction:
         self._throttle = data.data
     
     def execute_cb(self, goal):
-        # helper variable
         r = rospy.Rate(0.5)
+
+        # Reset Throttle, so we don't use previous value
+        self._throttle = 0.0
          
         # publish info to the console for the user
         rospy.loginfo(f"{self._action_name}: Starting")
            
         while True:
             if self._as.is_preempt_requested():
+                self._thrust_vec.publish(0.0)
                 rospy.loginfo(f"{self._action_name}: Preempted")
+
+                self._as.set_preempted()
                 break
 
-            self._thrust_vec.publisher(self._throttle)
+            self._thrust_vec.publish(self._throttle)
         
             r.sleep()
          
+
+if __name__ == '__main__':
+    rospy.init_node('manual_control_server')
+    server = ManualControlAction(rospy.get_name())
+    rospy.spin()
