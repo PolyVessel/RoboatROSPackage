a389e26a18431bd3ac76da31f9df235ba6f0bebc
Dylan Starink <dylan@starink.com>
2023-03-11 10:22:05 -0800

    Send Signal to Arduino not motor

--- src/drivers/rotor.py
+++ src/drivers/rotor.py
@@ -1,5 +1,6 @@
 import typing
 import time
+import serial
 
 class IMUException(Exception):
     pass
@@ -8,46 +9,24 @@ class Rotor:
 
     initialized = False
 
-    def __init__(self, channel: int):
-        self.channel = channel
-        self.init()
-
-    def init(self):
-        from rcpy.servo import ESC
-        import rcpy.servo as servo
-        import rcpy
-
-        # Disable the power rail on the servos channels
-        # for safety
-        servo.disable()
-
-        rcpy.set_state(rcpy.RUNNING)
-        self.esc = ESC(self.channel)
-
-        # Wait for PRU Init
-        time.sleep(1)
-
-        # Start the Clock for the escs
-        self.esc.start(0.02)
-
-        # Wait for PRU Init
-        time.sleep(1)
-
-        # Arm ESC
-        self.set(-0.1)
-        
-        # Blocking call, but in the future should probably use coroutines
-        time.sleep(1)
-
-        self.esc.set(0)
+    def __init__(self, serial_port: str):
+        self.serial_port = serial_port
+        self.serial = serial.Serial(serial_port, baudrate=9600, timeout=3)
 
 
-    def set(self, duty: float):
-        self.esc.start(0.02)
-        self.esc.set(duty)
+    '''
+    duty: 0 - 180
+    '''
+    def set(self, dutyLeft: int, dutyRight: int):
+        a = bytearray(b'')
+        a.append(dutyLeft)
+        a.append(b' ')
+        a.append(dutyRight)
+
+        self.serial.write(a)
 
     def off(self):
-        self.esc.set(0)
+        self.serial.write(b'0 0')
     
     def __del__(self):
         self.off()
